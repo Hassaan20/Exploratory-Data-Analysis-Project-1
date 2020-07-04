646f09d283fa99d9c43b5d4792cc783538840f18
Hassaan20 <67810219+Hassaan20@users.noreply.github.com>
2020-07-05 02:29:35 +0500

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -4,11 +4,8 @@ setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project/data")
 
 #Reads in data from file then subsets data for specified dates
 powerDT <- data.table::fread(input = "household_power_consumption.txt" , na.strings="?")
-
 powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
-
 powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
-
 powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
 
 png("plot4.png", width=480, height=480)
