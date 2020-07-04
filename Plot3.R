b90594184957d2314b7f661e08915633c4e1e9d0
Hassaan20 <67810219+Hassaan20@users.noreply.github.com>
2020-07-05 02:27:50 +0500

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -2,13 +2,9 @@ library("data.table")
 
 setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project/data")
 
-powerDT <- data.table::fread(input = "household_power_consumption.txt"
-                             , na.strings="?")
-
+powerDT <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
 powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
-
 powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
-
 powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
 
 png("plot3.png", width=480, height=480)
